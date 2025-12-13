80775c57b289ef9f0ce07992011d6fa5fbd7854e
Jeremiah Thomas <jeremiahjthomas21@gmail.com>
2025-12-13 06:25:19 -0600

    initial code with basic set ups

---
+++ colonist.py
@@ -0,0 +1,47 @@
+
+#FOR MODE w/ 4 PLAYERS
+
+"""
+This will ideally return:
+    Best placement for settlements 1, 2 and direction of road
+    Best option given a state of a game
+"""
+
+
+class gameState:
+    board = [[""]*3,[""]*4,[""]*5,[""]*3]
+    def updateState():
+        def clear():
+
+
+turn_position = input("Enter turn Position (1-4):")
+
+ranked_materials = [] #rank from 1-5
+
+class Nodes:
+    pings_around = 0
+    has_three = false
+    has_two = false
+    if has_three:
+        materials = []*3
+        #get materials around
+    if has_two:
+        materials = []*2
+        #get materials around
+    def get_materials_around(Node) => Array:
+        #idk how we'd iplement
+        return None
+
+#want to take everything into account (weak materials, turn_position, etc)
+
+if turn_position = 1:
+    #sort nodes by max pings
+    #favor ore wheat sheep
+    #else target brick wood
+    #port position
+
+if turn_position = 2:
+if turn_position = 3:
+if turn_position = 4:
+
+
